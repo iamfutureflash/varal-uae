fe7fec8c90712ce77f5e9a28ccb10c259175ed36
iamfutureflash <xenode2000@gmail.com>
2023-01-23 16:55:48 +0530

    for small screen navbar updated z index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
       <div className=''>
         <img src={varal_uae} alt="varal uae logo" className='' />
       </div>
-      <div className='for-small-screen md:hidden '>
+      <div className='for-small-screen md:hidden z-10 '>
         <img src={toggle?close:menu} alt="menu" className='w-10 h-10 relative' onClick={()=>setToggle((toggle)=>!toggle)} />
-        <div className={`for-small-screen-visible-on-click ${toggle?'flex':'hidden'}  absolute flex-col justify-center items-center space-x-4  right-10   slider_gradient rounded-lg mt-5 sidebar`}>
-          <ul className='flex flex-col  justify-center item-center   text-white capitalize  text-lg font-noto font-medium leading-10'>
-            <li className='cursor-pointer'>home</li>
-            <li className='cursor-pointer'>service</li>
-            <li className='cursor-pointer'>pricing</li>
-            <li className='cursor-pointer'>about us</li>
+        <div className={`for-small-screen-visible-on-click ${toggle?'flex':'hidden'}  absolute flex-col justify-center items-center space-x-4  left-4 right-4    slider_gradient rounded-lg mt-5 sidebar`}>
+          <ul className='flex flex-col  justify-center item-center   text-white capitalize  text-xl font-noto font-medium leading-10 w-[90vw]'>
+            <li className='cursor-pointer text-center py-2'>home</li>
+            <li className='cursor-pointer text-center py-2'>service</li>
+            <li className='cursor-pointer text-center py-2'>pricing</li>
+            <li className='cursor-pointer text-center py-2'>about us</li>
           </ul>
           <button className='hover:bg-primary text-white px-1 py-1 hover:px-1 hover:py-1 rounded-sm shadow-sm text-lg font-noto cursor-pointer font-medium leading-10'>Start a Company</button>
         </div>
